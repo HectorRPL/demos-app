4288c5271f18dfb32045555865f9cbee8b835f96
jalevtz <j.vlt.mtz@gmail.com>
2017-01-06 17:28:29 -0600

    Se modifica el path de archivo usuarios.js

--- imports/api/usuarios/usuarios.js
+++ imports/api/usuarios/usuarios.js
@@ -1,6 +1,9 @@
 import {Meteor} from 'meteor/meteor';
 import {Accounts} from 'meteor/accounts-base';
-import {Candidatos} from './candidatos/collection'
+import {Candidatos} from '../candidatos/collection'
+import {BitacoraLogin} from '../bitacoraLogin/collection'
+const LOGIN_METHOD = 'login';
+const CREATE_USER_METHOD = 'createUser';
 
 if (Meteor.isServer) {
     Meteor.publish('usersData', function () {
@@ -29,10 +32,41 @@ if (Meteor.isServer) {
             candidato.email = user.emails[0].address || '';
         }
         candidato.propietario = user._id;
-        user.estado = 'direccion';
         user.username = candidato.email;
         Candidatos.insert(candidato);
 
         return user;
     });
+
+    Accounts.onLogin((result)=> {
+        let bitacoraLogin = {
+            propietario: result.user._id,
+            fechaLogin: new Date(),
+            conexion: result.connection,
+            estadoRegistro: 'inicio.registro.direccion',
+            tipoLogin: result.type
+
+        };
+        if ('facebook' == result.type) {
+            bitacoraLogin.estadoRegistro = 'inicio.registro.facebook';
+            const bitacora = BitacoraLogin.findOne({propietario: result.user._id});
+            if (bitacora) {
+                BitacoraLogin.update({_id: bitacora._id}, {$set: {fechaLogin: new Date()}});
+            } else {
+                BitacoraLogin.insert(bitacoraLogin);
+            }
+        } else {
+            if (CREATE_USER_METHOD === result.methodName) {
+                BitacoraLogin.insert(bitacoraLogin);
+            }
+            if (LOGIN_METHOD === result.methodName) {
+                BitacoraLogin.update({propietario: result.user._id}, {$set: {fechaLogin: new Date()}});
+            }
+        }
+
+
+
+    });
+
+
 }
